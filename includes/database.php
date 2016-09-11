784dc91a388386657f70f9b2e06dfd8ab225857d
Indomedia <indomedia.contact@gmail.com>
2016-09-12 05:54:56 +0700

    Permulaan

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,16 @@
 <?php
 try{
-  $db = new PDO("mysql:host=localhost;dbname=siaka;charset=utf8mb4","root", "beta");  
-} catch (Exception $ex) {
-    echo 'gagal '.$ex.getMessage();
+	$db=new PDO("mysql:host=localhost;dbname=siakad;charset=utf8mb4","root","beta");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $ex){
+	exit("Tidak dapat menyambungkan dengan database! ".$ex->getMessage());
 }
 
-$db->prepare("insert into mahasiswa(id_mahasiswa, nim, alamat) values (?, ?, ?)");
-$db->execute(array('1', '104274026', 'Klampeyan RT 01 RW 03'));
+
+
+//$sql="INSERT INTO mahasiswa(id_mahasiswa, nim, alamat) VALUES (?, ?, ?)";
+//$stmt = $db->prepare($sql);
+//$stmt->execute(array(1,2,3));
 $db->query("select * from mahasiswa");
 foreach ($db->fetch(PDO::FETCH_BOTH) as $row){
     echo $row['nim'];
