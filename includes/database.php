beb189f97c66ebfcaec8e27ca0ce0a8ab353ec68
Indomedia <indomedia.contact@gmail.com>
2016-09-12 18:36:29 +0700

    Permulaan

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,9 @@
 <?php
 
 class Connection {
+
     protected $db;
+
     public function Connection() {
         $conn = NULL;
         try {
@@ -12,16 +14,26 @@ class Connection {
         }
         $this->db = $conn;
     }
+
     public function getConnection() {
         return $this->db;
     }
-    public function tambah($tabel, $kolom, $data) {
-        $tag = ltrim(str_repeat(",?", count($data)), ",");
-        $sql = "INSERT INTO $tabel($kolom) VALUES ($tag)";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($data);
+
+    public function tambah($array) {
+        try {
+            $value = array_values($array);
+            $tabel = $value[0];
+            $data_key = implode(",", array_keys($value[1]));
+            $data_value = array_values($value[1]);
+            $count = count($data_value);
+            $tag = ltrim(str_repeat(",?", $count), ",");
+            $sql = "INSERT INTO $tabel($data_key) VALUES ($tag)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute($data_value);
+            echo 'Berhasil';
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
     }
 
 }
-
-?>
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 include 'includes/database.php';
-echo 'kkk';
-$tag=  ltrim(str_repeat(",?", 5), ",");
-echo $tag;
+    $con = new Connection();
+    $con->getConnection();
+    $data=array("10", "89", "Salatiga");
+    //$con->tambah("mahasiswa", "id_mahasiswa, nim, alamat", $data);
+    $a=array("tabel"=>"mahasiswa",
+        "data"=>array(
+                        "id_mahasiswa"=>11, 
+                        "nim"=>104275026, 
+                        "alamat"=>"Salatiga"
+                        )
+        );
+    $value= array_values($a);
+    $count = count($a);
+    $tabel=$value[0];
+    $data_key = implode(",", array_keys($value[1]));
+    $data_value = implode(",", array_values($value[1]));
+    $con->tambah($a);
+   
+    
+
