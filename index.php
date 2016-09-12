496deacb85393d3bcef7c417bb922637b3e205fb
Indomedia <indomedia.contact@gmail.com>
2016-09-12 19:01:44 +0700

    Permulaan

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
-include 'includes/database.php';
-    $con = new Connection();
-    $con->getConnection();
-    $data=array("10", "89", "Salatiga");
-    //$con->tambah("mahasiswa", "id_mahasiswa, nim, alamat", $data);
-    $a=array("tabel"=>"mahasiswa",
-        "data"=>array(
-                        "id_mahasiswa"=>11, 
-                        "nim"=>104275026, 
-                        "alamat"=>"Salatiga"
-                        )
-        );
-    $value= array_values($a);
-    $count = count($a);
-    $tabel=$value[0];
-    $data_key = implode(",", array_keys($value[1]));
-    $data_value = implode(",", array_values($value[1]));
-    $con->tambah($a);
-   
-    
 
+include 'includes/database.php';
+$con = new Connection();
+$con->getConnection();
+$data = array("10", "89", "Salatiga");
+//$con->tambah("mahasiswa", "id_mahasiswa, nim, alamat", $data);
+$array = array("tabel" => "mahasiswa",
+    "data" => array(
+        "id_mahasiswa" => 11,
+        "nim" => 104275026,
+        "alamat" => "Salatiga"
+    )
+);
+$con->tambahData($array);
